a5d72821b28bc1d36b1319410de9b4456a45e02a
AstghikNnn <astghiknikoghosyann@gmail.com>
2023-09-11 21:07:52 +0400

    Homework-of-lesson-21

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,19 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
-    @Test
-    public void myFirstTestCase() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        ChromeDriver driver=new ChromeDriver();
-        driver.get("https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=100");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        driver.quit();
-    }
+
     @Test
     public void MySecondTestcase(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
@@ -25,7 +18,62 @@ public class MyFirstTest {
             throw new RuntimeException(e);
         }
         driver1.quit();
-
-
+    }
+    @Test
+    public void loginGitHub() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("https://github.com/login");
+        Thread.sleep(2000);
+        WebElement emailElement=driver.findElement(By.id("login_field"));
+        emailElement.sendKeys("AstghikNik");
+       WebElement password=driver.findElement(By.id("password"));
+       password.sendKeys("astastast95");
+       WebElement loginElement=driver.findElement(By.name("commit"));
+        loginElement.click();
+        driver.quit();
+    }
+    @Test
+    public void invalidpassword() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("https://github.com/login");
+        Thread.sleep(2000);
+        WebElement emailElement=driver.findElement(By.id("login_field"));
+        emailElement.sendKeys("AstghikNik");
+        WebElement password=driver.findElement(By.id("password"));
+        password.sendKeys("---95");
+        WebElement loginElement=driver.findElement(By.name("commit"));
+        WebElement errorElement = driver.findElement(By.className("js-flash-alert"));
+        Assert.assertEquals(errorElement.getText(), "Incorrect username or password.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        loginElement.click();
+        driver.quit();
+    }
+    @Test
+    public void emptypassword() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("https://github.com/login");
+        WebElement emailElement=driver.findElement(By.id("login_field"));
+        emailElement.sendKeys("nikoghosyan95@bk.ru");
+        WebElement password=driver.findElement(By.id("password"));
+        password.sendKeys("");
+        WebElement signinElement=driver.findElement(By.name("commit"));
+        signinElement.click();
+        WebElement errorElement = driver.findElement(By.className("js-flash-alert"));
+        Assert.assertEquals(errorElement.getText(), "Incorrect username or password.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        signinElement.click();
+        driver.quit();
     }
 }
+
